75d4ba4e167db5f17c614c45a17c55ad1f403518
sweetsour01 <33989302+sweetsour01@users.noreply.github.com>
2018-01-24 15:56:22 -0800

    First Commit Uploading the all files

---
+++ GitDemo/src/demo/git/com/GitPractice.java
@@ -0,0 +1,14 @@
+package demo.git.com;
+
+import org.testng.annotations.Test;
+
+public class GitPractice 
+{
+	@Test
+	public void pushPullfromGit()
+	{
+		System.out.println("Need to push to GIT repository");
+		
+	}
+
+}
